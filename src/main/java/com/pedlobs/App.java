1fa75fffa617e5006af5282fc687600887fe8b6b
pedlobs <pedlobs@gmail.com>
2026-02-25 23:33:45 +0100

    Initial commit

---
+++ src/main/java/com/pedlobs/App.java
@@ -0,0 +1,13 @@
+package com.pedlobs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
